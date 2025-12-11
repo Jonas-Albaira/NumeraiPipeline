4c87667a1618a73261ae669aeee17b70cd1b060d
Jonas Albaira <jonas.albaira@uoit.net>
2025-12-11 18:13:57 -0500

    Clear model UUID and API keys in predict_and_submit.py
    Removed sensitive information from the script.

--- predict_and_submit.py
+++ predict_and_submit.py
@@ -1,7 +1,7 @@
 from numerapi import NumerAPI
 
-model_uuid = "32601aab-e6d4-432c-aca4-6c36e8ab3691"  # store your Numerai model UUID here
-napi = NumerAPI(public_id="LXRJTLL43UJTW27LDO4PWUYBRSW5MQAC", secret_key="EJLIK4JF343K224QHSNFMI73WNR55RMEEU3ESFLCITY2B2J5UG4J7Q6U2ZEQ4XIR")
+model_uuid = ""  # store your Numerai model UUID here
+napi = NumerAPI(public_id="", secret_key="")
 
 # Upload predictions
 napi.upload_predictions("submission.csv", model_id=model_uuid)
