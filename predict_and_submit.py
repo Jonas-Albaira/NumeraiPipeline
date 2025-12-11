b38fe139508d7b7c00274f3b043fd268c4d7462d
Jonas Albaira <jonas.albaira@uoit.net>
2025-12-11 05:24:58 -0500

    upload

--- predict_and_submit.py
+++ predict_and_submit.py
@@ -20,7 +20,7 @@ submission.to_csv("submission.csv", index=False)
 print("Saved submission.csv")
 
 # ---- Upload ----
-sapi = NumerAPI(public_id="YOUR_ID", secret_key="YOUR_SECRET")
-sapi.upload_predictions("submission.csv", model_id="YOUR_MODEL_UUID")
+sapi = NumerAPI(public_id="DXU2EQ3UTLV4XBZ2NFKNXP3T5UE5I7Q3", secret_key="ABPTLCXD4OQL2YJ4TRO755474E3C5D2KSQQA7WKGWSDXF2M3ZUMEA6ZKVVUXRIS2")
+sapi.upload_predictions("submission.csv", model_id="32601aab-e6d4-432c-aca4-6c36e8ab3691")
 
 print("Uploaded!")
